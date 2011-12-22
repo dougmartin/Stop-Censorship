123e80fe2257154814da25639607ed3670fcac13
Christopher Davis <cdavis999@gmail.com>
2011-12-22 09:35:07 -0600

    A WordPress plugin to add stopcensorship.js to your blog

---
+++ stop-censorship.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Stop Censorsihp
+Author: Doug Martin
+Description: Add's Doug Martin's stopcensorship.js to your WordPress website.
+*/
+
+add_action( 'wp_enqueue_scripts', 'stop_censorship_enqueue_scripts' );
+function stop_censorship_enqueue_scripts()
+{
+    wp_enqueue_script(
+        'stop-censorship',
+        plugin_dir_url( __FILE__ ) . 'stopcensorship.js',
+        array(),
+        NULL,
+        true
+    );
+}
